49ca7bb2bfe2f12c59de1eaae9ef7d2fd5ba47ae
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-03-15 09:13:47 +1300

    Update example to demonstrate missed line.

--- examples/coverage/test.rb
+++ examples/coverage/test.rb
@@ -1,8 +1,16 @@
-
-puts "Hello World"
-
-all_the_things = []
-
-if all_the_things.any?
-	puts "I've got all the things"
+trace_point = TracePoint.new(:call, :return, :line, :c_call, :c_return, :b_call, :b_return) do |trace|
+	puts [trace.path, trace.lineno].join(":")
 end
+
+trace_point.enable
+
+values = {foo: 10}
+
+def shell_escape(x)
+	x
+end
+
+values.map{|key, value| [
+	key.to_s.upcase,
+	shell_escape(value) # TracePoint is never triggered for this line.
+]}
--- examples/coverage/tracepoint.rb
+++ examples/coverage/tracepoint.rb
@@ -1,6 +1,5 @@
 
-
-trace_point = TracePoint.new(:call, :line) do |trace|
+trace_point = TracePoint.new(:call, :return, :line, :c_call, :c_return, :b_call, :b_return) do |trace|
 	puts [trace.path, trace.lineno].join(":")
 end
 
