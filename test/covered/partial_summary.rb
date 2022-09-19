395e7a44b0836607fdf65990186656550d450b26
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-09-19 16:11:27 +1200

    Fix updated tests.

--- test/covered/partial_summary.rb
+++ test/covered/partial_summary.rb
@@ -14,20 +14,20 @@ describe Covered::PartialSummary do
 	let(:io) {StringIO.new}
 	
 	it "can generate partial summary" do
-		files.mark(__FILE__, 37, 1)
-		files.mark(__FILE__, 38, 0)
+		files.mark(__FILE__, 22, 1)
+		files.mark(__FILE__, 23, 0)
 		
 		summary.call(files, io)
 		
 		expect(io.string).not.to be =~ /#{first_line}/
-		expect(io.string).to be =~ /What are some of the best recursion jokes/
+		expect(io.string).to be(:include?, "What are some of the best recursion jokes?")
 	end
 	
 	it "should break segments with elipsis" do
 		files.mark(__FILE__, 1, 0)
 		files.mark(__FILE__, 2, 1)
 		
-		files.mark(__FILE__, 50, 0)
+		files.mark(__FILE__, 30, 0)
 		
 		summary.call(files, io)
 		
