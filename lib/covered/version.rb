374f3b9eb7b405c840bb9d1a063ecbb11896c1e7
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-06-30 22:53:36 +0900

    Modernize gem.

--- lib/covered/version.rb
+++ lib/covered/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2018-2022, by Samuel Williams.
+# Copyright, 2018-2023, by Samuel Williams.
 
 module Covered
 	VERSION = "0.20.2"
