c4604840b9a4afb6f7f0dafca63092b25f40f5fc
Samuel Williams <samuel.williams@shopify.com>
2026-06-15 00:11:35 +1200

    Improve adapter integration coverage

---
+++ fixtures/autostart/report.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2026, by Samuel Williams.
+
+puts "autostart fixture"
---
+++ fixtures/sus/dummy.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2026, by Samuel Williams.
+
+describe "Hello World Sus Test" do
+	it "can hello world" do
+		expect("Hello World").to be == "Hello World"
+	end
+end
